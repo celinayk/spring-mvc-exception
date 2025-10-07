f97c5f48085eeffe78a7143e353878bfd1f672ee
YeonKyung Ryu <celina16161616@gmail.com>
2025-10-07 17:33:30 +0900

    HandlerExceptionResolver 사용하여 예외처리

---
+++ src/main/java/hello/exception/exhandler/ErrorResult.java
@@ -0,0 +1,12 @@
+package hello.exception.exhandler;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorResult {
+  //예외가 발생했을 때 API 응답으로 사용하는 객체 정의
+  private String code;
+  private String message;
+}
